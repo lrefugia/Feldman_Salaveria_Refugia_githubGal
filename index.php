20361009bd3512522048c48caefa48a04b45a0e6
lrefugia <lrefugia@outlook.com>
2016-04-06 15:30:26 -0400

    Deleted excess code

--- index.php
+++ index.php
@@ -48,45 +48,6 @@
             <button data-slide="3"><span class="show-for-sr">Fourth slide.</span></button>
           </nav>
         </div>
-
-
-        <!--<div class="orbit-container show-for-small-up">
-          <ul data-orbit="" data-options="animation:slide;navigation_arrows:true;">
-            <li class="active">
-              <img src="images/ArmyOfDarkness.jpg" alt="Army of Darkness">
-            </li>
-            <li>
-              <img src="images/Beetlejuice.jpg" alt="Beetlejuice">
-            </li>
-            <li>
-              <img src="images/BlackHawkDown.jpg" alt="Black Hawk Down">
-            </li>
-          </ul>
-        </div>-->
-
-
-
-        
-        <!--<ul class="movie-orbit" data-orbit>
-          <li class="active">
-            <img src="images/ArmyOfDarkness.jpg" alt="Army of Darkness" />
-            <div class="orbit-caption">
-              Army of Darkness
-            </div>
-          </li>
-          <li>
-            <img src="images/Beetlejuice.jpg" alt="Beetlejuice" />
-            <div class="orbit-caption">
-              Beetlejuice
-            </div>
-          </li>
-          <li>
-            <img src="images/BlackHawkDown.jpg" alt="Black Hawk Down" />
-            <div class="orbit-caption">
-              Black Hawk Down
-            </div>
-          </li>
-        </ul>-->
       
       </div>
     </div>
